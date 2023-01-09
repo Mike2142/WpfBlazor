94b14d591ae81dad47f89f51bcc09530050aff07
mkolyadin <MKolyadin@amdocs.com>
2023-01-09 07:19:53 +0300

    WPF app

--- WpfBlazor/MainWindow.xaml.cs
+++ WpfBlazor/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WpfBlazor
 {
@@ -23,6 +24,10 @@ namespace WpfBlazor
         public MainWindow()
         {
             InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
         }
     }
 }
